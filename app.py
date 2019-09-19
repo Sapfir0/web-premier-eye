4cf8ed2bc99c28daaa92bf14240a1cad7482e1c6
Sapfir0 <sapfir999999@yandex.ru>
2019-09-19 23:54:15 +0400

    new port

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from application import createApp
 from config import Config as cfg
+import os
 
 app = createApp()
 
 if __name__ == "__main__":  # хм это же мейн
-    app.run(cfg.FLASK_RUN_HOST, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(cfg.FLASK_RUN_HOST, port=port, debug=True)
