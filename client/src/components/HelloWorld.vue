4a10a92720aa0f3be43de8caaa1af2ea0dbfa45c
Sapfir0 <sapfir999999@yandex.ru>
2019-10-06 01:10:00 +0400

    таймлайн

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -6,22 +6,28 @@
     >
 
 
-    <v-list camers>
-      <v-list-item v-for="i in camersCount" @click="">
-          <v-list-item-content>
-            <v-list-item-title>Камера №{{ i }}</v-list-item-title>
-          </v-list-item-content>
-      </v-list-item>
-    </v-list>
+    <v-container>
+      <v-layout row>
+        <v-flex md6>
+          <v-list camers>
+            <v-list-item v-for="i in camersCount" @click="">
+              <v-list-item-content>
+                <v-list-item-title >Камера №{{ i }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-flex>
+        <v-flex md6>
+          <v-flex xs12>
+            <v-img src='../../../server/application/static/uploads/s.png' contain height="100%" />
+            </v-flex>
+              <v-slider v-model="value" :step="imagesCount" ticks></v-slider>
+        </v-flex>
+      </v-layout> 
+    </v-container>
 
-  
 
-      <v-flex xs12>
-        <h1 class="display-2 font-weight-bold mb-3">
-          Блок видео
-        </h1>
-      </v-flex>
-
+ 
 
     </v-layout>
   </v-container>
@@ -33,7 +39,8 @@ import Vue from 'vue';
 export default Vue.extend({
 data() {
   return {
-    camersCount: 5,
+    camersCount: 5, 
+    imagesCount: 25, //ВАЖНО!!! сделать этот параметр прилетающим из дсона с сервера
   }
 }
 });
