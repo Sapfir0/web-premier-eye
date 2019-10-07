c82c5f80d4a1488620003f34ca42f10ace608b88
Sapfir0 <sapfir999999@yandex.ru>
2019-10-07 11:17:36 +0400

    Обширный fix

--- src/components/Ping.vue
+++ src/components/Ping.vue
@@ -1,12 +1,9 @@
 <template>
   <div class="team">
     <h1>Ping</h1>
-  <h1>1 {{ json }} </h1>
   <h2>2 {{images}} </h2>
     <v-img v-bind:src="images" contain height="100%" />
-          
-     
-          
+                 
   </div>
 </template>
 
@@ -14,12 +11,9 @@
 import Vue from 'vue';
 
 export default Vue.extend({
-async data() {
-    const localhost = "http://localhost:5000/gallery"
-    const response = await fetch(localhost);
-    const { json } = await response.json()
+data() {
   return {
-    images: "http://localhost:5000/gallery/s.png",
+    images: "http://localhost:5000/gallery/image11.jpg",
   }
 },
 methods: {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,41 @@
 <template>
-  <HelloWorld />
+  <v-container>
+    <v-layout
+      text-center
+      wrap
+    >
+
+    <v-container>
+      <v-layout row>
+        <v-flex md6>
+          <CamersList />
+        </v-flex>
+        <v-flex md6>
+          <v-flex xs12>
+            <v-img src='http://localhost:5000/gallery/s.png' contain height="100%" />
+            </v-flex>
+              <v-slider :step="count" ticks></v-slider>
+        </v-flex>
+      </v-layout> 
+    </v-container>
+ 
+
+    </v-layout>
+  </v-container>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from '../components/HelloWorld.vue';
+import CamersList from '../components/CamersList.vue'
 
 export default Vue.extend({
+data() {
+  return {
+    count: 5, // 5%
+  }
+},
   components: {
-    HelloWorld,
+    CamersList,
   },
 });
 </script>
