1e9423df0a7e4b19e9813210470bf14f289fd0ef
Sapfir0 <sapfir999999@yandex.ru>
2019-10-13 15:15:46 +0400

    Похоже я сломал лист

--- src/components/Slide.vue
+++ src/components/Slide.vue
@@ -1,53 +1,37 @@
 <template>
 
-  <v-flex md6>
-    <v-list camers > <!--style="width:30%;" -->
-         <v-list-item v-for="i in camersCount"  >
-                  <v-list-item-content @click="pingRouter(i)">
-                  <v-list-item-title >Камера № {{ i }}</v-list-item-title>
-                  </v-list-item-content>
-         </v-list-item>
-     </v-list>
-    <v-flex xs12>
+  <v-flex>
+
+      <v-list camers flat>
+           <v-list-item v-for="i in camersCount"  >
+                    <v-list-item-content @click="pingRouter(i)">
+                    <v-list-item-title >Камера № {{ i }}</v-list-item-title>
+                    </v-list-item-content>
+           </v-list-item>
+       </v-list>
+
       <v-carousel
         v-model="model"
         hide-delimiters
         :show-arrows="false"
-        :continuous="false"
-        style="width:100%">
-        <v-carousel-item
-          v-for="(filename) in camers"
-          :key="filename"
-          :src="getImage(filename)"
-          alt="Изображение с камеры"
-          style="width:100%;height:100%;"
-          :reverse-transition="false"
-          :transition="false"
-        >
-<!--          <v-sheet-->
-<!--            v-if="foo()"-->
-<!--          color="red"-->
-<!--          height="100%"-->
-<!--          tile-->
-<!--        >-->
-<!--          <v-row-->
-<!--            class="fill-height"-->
-<!--            align="center"-->
-<!--            justify="center"-->
-<!--          >-->
-<!--            <div class="display-3">Камера недоступна</div>-->
-<!--          </v-row>-->
-<!--        </v-sheet>-->
-        </v-carousel-item>
+        :continuous="false">
+          <v-carousel-item
+            v-for="(filename) in camers"
+            :key="filename"
+            :src="getImage(filename)"
+            alt="Изображение с камеры"
+            :reverse-transition="false"
+            :transition="false"
+          >
+          </v-carousel-item>
       </v-carousel>
-            </v-flex>
-                <v-slider v-model="slider"
-                :min="0"
-                :max="imagesInCamerN-1"
-                @input="model=slider"
-                ticks> </v-slider>
 
-            </v-flex>
+      <v-slider v-model="slider"
+      :min="0"
+      :max="imagesInCamerN-1"
+      @input="model=slider"
+      ticks> </v-slider>
+  </v-flex>
 </template>
 
 <script>
@@ -63,28 +47,13 @@ export default {
     };
   },
   methods: {
-    // async checkCamer(camerId) {
-    //   const localhost = `http://localhost:8050/gallery/camera/${camerId}`;
-    //   try {
-    //     const response = await fetch(localhost);
-    //     const { status } = response;
-    //     console.log("я вывелся", status);
-    //     if (status === 404) {
-    //       const { statusText } = response;
-    //       return false;
-    //     }
-    //   } catch (e) {
-    //     console.log(e);
-    //   }
-    // },
-
     async pingRouter(camerId) {
       const localhost = `http://localhost:8050/gallery/camera/${camerId}`;
       const response = await fetch(localhost);
       const { status } = response;
       if (status === 404) {
         const { statusText } = response;
-        console.log(statusText);
+        console.log('Статус', statusText);
         return false;
       }
       const json = await response.json();
