67f33013ac9e98207b91f35bbb00e7872f80d03c
Sapfir0 <sapfir999999@yandex.ru>
2019-10-27 21:48:19 +0400

    Вынес просмотр в отельный компонент

--- src/components/Slide.vue
+++ src/components/Slide.vue
@@ -30,28 +30,7 @@
                 </v-card>
             </v-col>
             <v-col cols="10" md="3">
-                <v-card class="mx-auto" outlined tile>
-                    <v-list>
-                        <v-subheader>{{ this.info.filename}}</v-subheader>
-                        <v-list-item>{{this.info.fixationDatetime}} </v-list-item>
-                        <v-list-item>Снято с камеры №{{this.info.numberOfCam}} </v-list-item>
-                        <v-list-item v-if="!this.info.objects">Объектов нет</v-list-item>
-
-                        <v-list-group v-for="obj in this.info.objects" color="primary" value="false">
-                            <template v-slot:activator >
-                                <v-list-item-icon v-show="obj.typeOfObject === 'car' "><v-icon>mdi-car</v-icon></v-list-item-icon>
-                                <v-list-item-icon v-show="obj.typeOfObject === 'person' "><v-icon>mdi-account</v-icon></v-list-item-icon>
-
-                                <v-list-item-title>{{obj.typeOfObject}}</v-list-item-title>
-                            </template>
-
-                            <v-list-item>
-                                <v-list-item-title>Степень уверенности: {{obj.scores.toFixed(2)}}</v-list-item-title>
-                            </v-list-item>
-                        </v-list-group>
-
-                    </v-list>
-                </v-card>
+                <ImageInfo :info="this.info" />
             </v-col>
         </v-row>
 
@@ -62,6 +41,7 @@
 <script>
     const routing = require('../router');
     import CamersList from '@/components/CamersList';
+    import ImageInfo from "@/components/ImageInfo";
 
     export default {
         name: 'Slide',
@@ -76,7 +56,7 @@
              };
         },
         components: {
-            CamersList,
+            CamersList, ImageInfo
         },
         methods: {
             async getInfoFromImage(filename) {
