3c64b6f31bee434f5eb040fc701eae8b0e89217c
Sapfir0 <sapfir999999@yandex.ru>
2019-11-25 02:31:03 +0400

    начал писать более обширные тесты

--- serverTest/serverTest/UnitTest1.cs
+++ serverTest/serverTest/UnitTest1.cs
@@ -1,18 +1,22 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 using NUnit.Framework;
 
 
 namespace serverTest {
     public class Tests {
-        private WebRequest _request;
+        static readonly HttpClient client = new HttpClient();
+        protected static readonly string ServerUrl = "http://localhost:8050/";
         
         [OneTimeSetUp]
         public void OneTimeSetup() {
-            _request = WebRequest.Create("http://localhost:8050/");
-            _request.Credentials = CredentialCache.DefaultCredentials;
         }
 
         [OneTimeTearDown]
@@ -23,33 +27,25 @@ namespace serverTest {
         [SetUp]
         public void Setup() {
         }
-
-        [Test]
-        public void IsServerAvailable() {
-            WebResponse response = _request.GetResponse();
-            Assert.AreEqual("OK", ((HttpWebResponse)response).StatusDescription);
-            response.Close();
-        }
-
-        public string readData() {
-            WebResponse response = _request.GetResponse();
-            string responseFromServer;
-            using (Stream dataStream = response.GetResponseStream())
-            {
-                // Open the stream using a StreamReader for easy access.  
-                StreamReader reader = new StreamReader(dataStream);
-                // Read the content.  
-                responseFromServer = reader.ReadToEnd();
-                // Display the content.  
-                Console.WriteLine(responseFromServer);
-            }
-            response.Close();
-            return responseFromServer;
-        }
         
-        [Test]
-        public void Test1() {
 
+        [Test]
+        public async Task IsStatusCodeIsOk() {
+            HttpResponseMessage response = await client.GetAsync(ServerUrl);  // кинет исключение если не найдет
+            Assert.IsTrue(response.EnsureSuccessStatusCode().StatusCode == HttpStatusCode.OK);
+        }
+
+
+        
+        [TestCase(ServerUrl+"gallery/camera")]
+        public async Task IsAllCamerasAvailable(string route) {
+            int camersCount = 5;
+            for (int camera = 1; camera < camersCount; camera++) {
+                HttpResponseMessage response = await client.GetAsync($"{route}/{camera}");
+                if (response.EnsureSuccessStatusCode().StatusCode != HttpStatusCode.OK) {
+                    Assert.Warn($"Camera {camera} is not found");
+                }
+            }
         }
     }
 }
---
+++ serverTest/serverTest/gallery.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace serverTest {
+    public class gallery : Tests {
+        [TestCase(ServerUrl+"gallery")]
+        public async Task GetAllImagesNotEmpty(string route) {
+            string response = await client.GetStringAsync(route);
+            var imagesList = JsonConvert.DeserializeObject<List<string>>(response);
+            Assert.IsNotEmpty(imagesList); // хотя бы одно изображение
+            
+        }
+        
+        [TestCase(ServerUrl+"gallery")]
+        public async Task GetAllImagesHaveSameElements(string route) {
+            string response = await client.GetStringAsync(route);
+            var imagesList = JsonConvert.DeserializeObject<List<string>>(response);
+            Assert.IsTrue(imagesList.Distinct().Skip(1).Any());
+        }
+    }
+}
