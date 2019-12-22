3bbe03da302c583ca56f217a07a0d0432a3b0acf
Sapfir0 <sapfir999999@yandex.ru>
2019-12-22 18:30:08 +0400

    Пересборка тестового модуля

---
+++ test/test/Gallery.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace serverTest {
+    public class Gallery : Base {
+        private const string routeUrl = ServerUrl + "gallery";
+        
+        [TestCase(routeUrl)]
+        public async Task GetAllImagesNotEmpty(string route) {
+            string response = await Client.GetStringAsync(route);
+            var imagesList = JsonConvert.DeserializeObject<List<string>>(response);
+            Assert.IsNotEmpty(imagesList); 
+            
+        }
+        
+        [TestCase(routeUrl)]
+        public async Task GetAllImagesHaveSameElements(string route) {
+            string response = await Client.GetStringAsync(route);
+            var imagesList = JsonConvert.DeserializeObject<List<string>>(response);
+            Assert.IsTrue(imagesList.Distinct().Skip(1).Any());
+        }
+    }
+}
---
+++ test/test/test/Base.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Drawing;
+using System.IO;
+using System.Text.Encodings.Web;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace serverTest {
+    public class Base {
+        protected const string ServerUrl = "http://localhost:8050/";
+        protected static readonly HttpClient Client = new HttpClient();
+
+        [Test]
+        public async Task IsServerIsOk() {
+            HttpResponseMessage response = await Client.GetAsync(ServerUrl);  // кинет исключение если не найдет ури
+            Assert.IsTrue(response.IsSuccessStatusCode);
+        }
+        
+    }
+}
